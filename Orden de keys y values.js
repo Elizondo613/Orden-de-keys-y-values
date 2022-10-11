cdc559c5d4e059665fa5fc2ae2a762c3b880d36d
Javier Elizondo <josejavierem@gmail.com>
2022-10-11 17:43:09 -0600

    Initial commit

---
+++ Orden de keys y values.js
@@ -0,0 +1,21 @@
+var obj = {1 :'a', 0 :'c', 2 :'b'};
+
+console.log("Arreglo sin ordenar: ");
+console.log(Object(obj));
+
+console.log("Arreglo de Keys: ");
+console.log(Object.keys(obj).sort());
+
+console.log("Arreglo de Values: ");
+console.log(Object.values(obj).sort());
+
+var obj = {b :'Microsoft', c :'Apple', a :'Google'};
+console.log("Arreglo sin ordenar: ");
+console.log(Object(obj));
+
+console.log("Arreglo de Keys: ");
+console.log(Object.keys(obj).sort());
+
+console.log("Arreglo de Values: ");
+console.log(Object.values(obj).sort());
+
